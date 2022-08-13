8b198f2f9c98c772aeca1a16ecba22cb051b5c0d
unknown <zcfshr@gmail.com>
2022-08-13 09:52:00 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var today = moment();
+$("#currentDay").text(today.format("MMM Do, YYYY"));
+
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    var formatted = h + ":" + m + ":" + s;
+    $("#txt").text(formatted);
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+  }
+  startTime();
