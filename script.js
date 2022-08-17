446291f090d97e2c90aa340a32efc44888b72117
unknown <zcfshr@gmail.com>
2022-08-17 17:58:43 -0600

    change color of past hours

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+
+
 var today = moment();
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
@@ -5,12 +8,59 @@ function startTime() {
     const today = new Date();
     let h = today.getHours();
     let m = today.getMinutes();
-    let s = today.getSeconds();
+    
     m = checkTime(m);
-    s = checkTime(s);
-    var formatted = h + ":" + m + ":" + s;
+   
+    var ampm = h >= 12 ? 'PM' : 'AM';
+    h = h % 12;
+    h = h ? h : 12;
+    
+    var formatted = h + ":" + m + " " + ampm;
     $("#clock").text(formatted);
     setTimeout(startTime, 1000);
+    
+if (h > 9 || ampm == 'PM') {
+  document.getElementById('nineAM').setAttribute('class', 'pastHour');
+  document.getElementById('9activity').textContent = "past 9 am";
+  };
+if (h > 10 || ampm == 'PM') {
+  document.getElementById('tenAM').setAttribute('class', 'pastHour');
+  };
+if (h > 11 || ampm == 'PM') {
+  document.getElementById('elevenAM').setAttribute('class', 'pastHour');
+  };
+if (h < 12 && ampm == 'PM') {
+    document.getElementById('twelvePM').setAttribute('class', 'pastHour');
+  };
+if (h > 1 && ampm == 'PM') {
+    document.getElementById('onePM').setAttribute('class', 'pastHour');
+  };
+if (h > 2 && ampm == 'PM') {
+    document.getElementById('twoPM').setAttribute('class', 'pastHour');
+  };
+if (h > 3 && ampm == 'PM') {
+  document.getElementById('threePM').setAttribute('class', 'pastHour');
+  };  
+if (h > 4 && ampm == 'PM') {
+    document.getElementById('fourPM').setAttribute('class', 'pastHour');
+  };  
+if (h > 5 && ampm == 'PM') {
+    document.getElementById('fivePM').setAttribute('class', 'pastHour');
+  };  
+if (h > 6 && ampm == 'PM') {
+    document.getElementById('sixPM').setAttribute('class', 'pastHour');
+  };
+if (h > 7 && ampm == 'PM') {
+    document.getElementById('sevenPM').setAttribute('class', 'pastHour');
+  };
+if (h > 8 && ampm == 'PM') {
+    document.getElementById('eightPM').setAttribute('class', 'pastHour');
+  }; 
+if (h > 9 && ampm == 'PM') {
+    document.getElementById('ninePM').setAttribute('class', 'pastHour');
+  };   
+  
+
   }
   
   function checkTime(i) {
@@ -18,3 +68,29 @@ function startTime() {
     return i;
   }
   startTime();
+
+
+renderLastRegistered();
+
+  var saveButton = document.getElementById("btn1");
+  var activityInput = document.getElementById("input-1");
+
+  function renderLastRegistered() {
+    var activity = localStorage.getItem("activity");
+    
+    //activityInput.textContent = activity.value;
+    //$("#input-1").text(activity);
+    console.log(activity);
+  };
+  
+  saveButton.on("click", function(event) {
+    event.preventDefault();
+  
+    var activity = document.getElementById("input-1").value;
+    localStorage.setItem("activity", activity);
+
+    
+    renderLastRegistered();
+    });
+  
+  
